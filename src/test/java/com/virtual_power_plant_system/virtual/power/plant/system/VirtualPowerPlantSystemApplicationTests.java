7cb42128bbc4cddc09dbe02ffd830e7ee0b8eed5
IsuruKwashalya <150815b@uom.lk>
2022-09-24 02:17:31 +0530

    Created

---
+++ src/test/java/com/virtual_power_plant_system/virtual/power/plant/system/VirtualPowerPlantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtual_power_plant_system.virtual.power.plant.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPowerPlantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
