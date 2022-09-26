2701ba005460a6dd8ae0dfd7f4030544ab6ba3b2
IsuruKwashalya <150815b@uom.lk>
2022-09-26 12:45:08 +0530

    otherChanges

--- src/main/java/com/virtual_power_plant_system/virtual/power/plant/system/VirtualPowerPlantSystemApplication.java
+++ src/main/java/com/virtual_power_plant_system/virtual/power/plant/system/VirtualPowerPlantSystemApplication.java
@@ -3,11 +3,11 @@ package com.virtual_power_plant_system.virtual.power.plant.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class VirtualPowerPlantSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VirtualPowerPlantSystemApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(VirtualPowerPlantSystemApplication.class, args);
 	}
-
 }
+
